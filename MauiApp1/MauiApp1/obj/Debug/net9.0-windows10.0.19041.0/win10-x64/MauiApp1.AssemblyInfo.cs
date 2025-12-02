3e42918a4948d30526829213ad660d40c9df41e0
NiceTMY68 <131985997+NiceTMY68@users.noreply.github.com>
2025-12-03 06:31:31 +0700

    xyz

--- MauiApp1/MauiApp1/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MauiApp1.AssemblyInfo.cs
+++ MauiApp1/MauiApp1/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MauiApp1.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+14dbec0a506eba6e1f0675fafd08969b9cc99bb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
